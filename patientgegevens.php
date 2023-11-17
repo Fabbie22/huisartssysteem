e34773927e16ddc7bd2ee574d8b7cb3f67c6a06c
Fabian Walter <fabianwalter8@gmail.com>
2023-11-17 21:23:28 +0100

    navbar update en verwijder
    Navbar zit nu een verschil in zodat er beter terug kan worden gegaan naar andere pagina. Ook JS gefixt, bovenaan doet die het, onderaan niet, miss nog kijken.

--- patientgegevens.php
+++ patientgegevens.php
@@ -22,70 +22,70 @@ $datum = date('Y/m/d');
   <title>HiX - Patient</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php"><i class="fa-solid fa-house-medical" style="color: #000000;"></i> HiX</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-      <li class="nav-item">
-          <a class="nav-link active" href="index.php"><i class="fa-solid fa-book-medical" style="color: #000000;"></i> Overzicht</a>
-        </li>
-      </ul>
-  </div>
-</nav>
-<?php
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php"><i class="fa-solid fa-house-medical" style="color: #000000;"></i> HiX</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="index.php"><i class="fa-solid fa-book-medical" style="color: #000000;"></i> Overzicht</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <?php
     if(isset($_GET['patient_id']))
-        {
-            $patient_id = $_GET['patient_id'];
-
-            $query = "SELECT * FROM patient WHERE patient_id=:patient_id";
-            $statement = $dbh->prepare($query);
-            $data = [':patient_id' => $patient_id];
-            $statement->execute($data);
-          
-            $result = $statement->fetch(PDO::FETCH_ASSOC);
-                        }
-                        ?>
+    {
+      $patient_id = $_GET['patient_id'];
+      
+      $query = "SELECT * FROM patient WHERE patient_id=:patient_id";
+      $statement = $dbh->prepare($query);
+      $data = [':patient_id' => $patient_id];
+      $statement->execute($data);
+      
+      $result = $statement->fetch(PDO::FETCH_ASSOC);
+    }
+    ?>
 <div class="container">
-<form action='addfunctions.php' method="POST" enctype="multipart/form-data">
-
-<input type="hidden" name="patient_id" value="<?=$result['patient_id']?>" />
-  <div class="row">
-            <div class="col-md-6">
-                <label for="voor_naam" class="form-label">Voornaam</label>
-                <input type="text" class="form-control" id="voor_naam" name="voor_naam" value="<?=$result['voor_naam']?>"  />
-            </div>
-            <div class="col-md-6">
-                <label for="achter_naam" class="form-label">Achternaam</label>
-                <input type="text" class="form-control" id="achter_naam" name="achter_naam" value="<?=$result['achter_naam']?>" />
-            </div>
-            <div class="col-md-8">
-                <label for="adres" class="form-label">Adres</label>
-                <input type="text" class="form-control" id="adres" name="adres" value="<?=$result['adres']?>" />
-            </div>
-            <div class="col-md-4">
-              <label for="huisnummer" class="form-label">Huisnummer</label>
-              <input type="text" class="form-control" id="huisnummer" name="huisnummer" value="<?=$result['huisnummer']?>" />
-            </div>
-            <div class="col-md-6">
-                <label for="postcode" class="form-label">Postcode</label>
-                <input type="text" class="form-control" id="postcode" name="postcode" value="<?=$result['postcode']?>" />
-            </div>
-            <div class="col-md-6">
-                <label for="plaats" class="form-label">Plaats</label>
-                <input type="text" class="form-control" id="plaats" name="plaats" value="<?=$result['plaats']?>" />
-            </div>
-            <div class="col-md-12">
-                <label for="telefoonnummer" class="form-label">Telefoonnummer</label>
-                <input type="text" class="form-control" id="telefoonnummer" name="telefoonnummer" value="<?=$result['telefoonnummer']?>" />
-            </div>
-            <div class="col-md-12">
-                <label for="actief" class="form-label">Activiteit</label>
-                <select name="actief" id="actief" class="form-select">
-                  <?php
+  <form action='addfunctions.php' method="POST" enctype="multipart/form-data">
+    
+    <input type="hidden" name="patient_id" value="<?=$result['patient_id']?>" />
+    <div class="row">
+      <div class="col-md-6">
+        <label for="voor_naam" class="form-label">Voornaam</label>
+        <input type="text" class="form-control" id="voor_naam" name="voor_naam" value="<?=$result['voor_naam']?>"  />
+      </div>
+      <div class="col-md-6">
+        <label for="achter_naam" class="form-label">Achternaam</label>
+        <input type="text" class="form-control" id="achter_naam" name="achter_naam" value="<?=$result['achter_naam']?>" />
+      </div>
+      <div class="col-md-8">
+        <label for="adres" class="form-label">Adres</label>
+        <input type="text" class="form-control" id="adres" name="adres" value="<?=$result['adres']?>" />
+      </div>
+      <div class="col-md-4">
+        <label for="huisnummer" class="form-label">Huisnummer</label>
+        <input type="text" class="form-control" id="huisnummer" name="huisnummer" value="<?=$result['huisnummer']?>" />
+      </div>
+      <div class="col-md-6">
+        <label for="postcode" class="form-label">Postcode</label>
+        <input type="text" class="form-control" id="postcode" name="postcode" value="<?=$result['postcode']?>" />
+      </div>
+      <div class="col-md-6">
+        <label for="plaats" class="form-label">Plaats</label>
+        <input type="text" class="form-control" id="plaats" name="plaats" value="<?=$result['plaats']?>" />
+      </div>
+      <div class="col-md-12">
+        <label for="telefoonnummer" class="form-label">Telefoonnummer</label>
+        <input type="text" class="form-control" id="telefoonnummer" name="telefoonnummer" value="<?=$result['telefoonnummer']?>" />
+      </div>
+      <div class="col-md-12">
+        <label for="actief" class="form-label">Activiteit</label>
+        <select name="actief" id="actief" class="form-select">
+          <?php
                   if($result['actief'] == 1){
                     echo "<option value=".$result['actief']." selected>Actief</option>";
                     echo "<option value='0'>Inactief</option>";
@@ -97,10 +97,10 @@ $datum = date('Y/m/d');
                   ?>
                 </select>            
               </div>
-        <button type="submit" name="update_button" class="btn btn-primary topgap2 col-md-6"><i class="fa-solid fa-floppy-disk" style="color: #ffffff;"></i> Opslaan</button>
-        <button type="button" class='btn btn-success topgap2 col-md-6' data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-plus" style="color: #ffffff;"></i> Notitie Toevoegen</button></a>
-
-    </form>
+              <button type="submit" name="update_button" class="btn btn-primary topgap2 col-md-6"><i class="fa-solid fa-floppy-disk" style="color: #ffffff;"></i> Opslaan</button>
+              <button type="button" class='btn btn-success topgap2 col-md-6' data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-plus" style="color: #ffffff;"></i> Notitie Toevoegen</button></a>
+              
+            </form>
   </div>
 </div>
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -112,39 +112,39 @@ $datum = date('Y/m/d');
       </div>
       <div class="modal-body">
         <form action="./addfunctions.php" method="post">
-        <input type="hidden" name="patient_id" value="<?=$result['patient_id']?>" />
-        <div class="row">
-          <div class="col-md-12">
-            <input type="text" class="form-control" placeholder="Onderwerp (max 50 tekens)" aria-label="Onderwerp" name="onderwerp" id="onderwerp" maxlength="50" required>
+          <input type="hidden" name="patient_id" value="<?=$result['patient_id']?>" />
+          <div class="row">
+            <div class="col-md-12">
+              <input type="text" class="form-control" placeholder="Onderwerp (max 50 tekens)" aria-label="Onderwerp" name="onderwerp" id="onderwerp" maxlength="50" required>
+            </div>
+            <div class="col-md-12 topgap">
+              <textarea class="form-control" placeholder="Hier je tekst (max 1000 tekens) " aria-label="Hier je tekst" name="tekst" id="tekst" maxlength="1000" required></textarea>
+            </div>
           </div>
-          <div class="col-md-12 topgap">
-            <textarea class="form-control" placeholder="Hier je tekst (max 1000 tekens) " aria-label="Hier je tekst" name="tekst" id="tekst" maxlength="1000" required></textarea>
-          </div>
-      </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Sluiten</button>
+          <button type="submit" class="btn btn-primary" name="notitietoevoegen">Notitie Toevoegen</button>
+        </div>
+      </form>
     </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Sluiten</button>
-      <button type="submit" class="btn btn-primary" name="notitietoevoegen">Notitie Toevoegen</button>
-    </div>
-  </form>
-</div>
-</div>
+  </div>
 </div>
 <div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">Notitie verwijderen</h1>
-            </div>
-            <div class="modal-body">
-                Weet je zeker dat je deze notitie wil verwijderen?
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-bs-dismiss="modal">Afbreken</button>
-                <a class="btn btn-danger btn-ok">Verwijderen</a>
-            </div>
-        </div>
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Notitie verwijderen</h1>
+      </div>
+      <div class="modal-body">
+        Weet je zeker dat je deze notitie wil verwijderen?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-bs-dismiss="modal">Afbreken</button>
+        <a class="btn btn-danger btn-ok">Verwijderen</a>
+      </div>
     </div>
+  </div>
 </div>
 <div class="container mt-3">
   <h2>Notities</h2>
@@ -152,25 +152,28 @@ $datum = date('Y/m/d');
   <?php
   $notitie = notities($dbh, $patient_id);
   
- if(!$notitie){
-  echo "Er zijn geen notities bekend bij deze patiënt";
- }
-
+  if(!$notitie){
+    echo "Er zijn geen notities bekend bij deze patiënt";
+  }
+  
   foreach($notitie as $notitiepatient){
     $notitie_notitie_id = $notitiepatient['notitie_notitie_id'];
     $patient_patient_id = $notitiepatient['patient_patient_id'];
     echo '<div class="card topgap2">
-      <h4 class="card-title">'.$notitiepatient['onderwerp'].'</h4>
-      <h6 class="card-title">Datum: <b>'.$mysqldate = date( 'd-m-Y', strtotime($notitiepatient['datum'] )).'</b></h6>
-      <div class="card-body">'.$notitiepatient['tekst'].'</div>
-      <button class="btn btn-danger" data-href=delete.php?notitie_notitie_id=' . $notitie_notitie_id. '&patient_patient_id='. $patient_patient_id.' data-bs-toggle="modal" data-bs-target="#confirm-delete"><i class="fa-solid fa-trash-can"></i></button>
+    <h4 class="card-title">'.$notitiepatient['onderwerp'].'</h4>
+    <h6 class="card-title">Datum: <b>'.$mysqldate = date( 'd-m-Y', strtotime($notitiepatient['datum'] )).'</b></h6>
+    <div class="card-body">'.$notitiepatient['tekst'].'</div>
+    <button class="btn btn-danger" data-href=delete.php?notitie_notitie_id=' . $notitie_notitie_id. '&patient_patient_id='. $patient_patient_id.' data-bs-toggle="modal" data-bs-target="#confirm-delete"><i class="fa-solid fa-trash-can"></i></button>
     </div>';
   }
   ?>
   </div>
   <script>  
+$(document).ready(function() {
   $('#confirm-delete').on('show.bs.modal', function(e) {
-    $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+    var hrefValue = $(e.relatedTarget).data('href');
+    $(this).find('.btn-ok').attr('href', hrefValue);
+  });
 });
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
